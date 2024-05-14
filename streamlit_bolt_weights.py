889551e23037442a5593f98177c48a709446d39e
Joel Welling <welling@psc.edu>
2024-05-13 21:45:06 -0400

    Improved messages. Fail if any value is high.

--- streamlit_bolt_weights.py
+++ streamlit_bolt_weights.py
@@ -5,10 +5,12 @@ import math
 from time import sleep
 
 max_wt = 16.05
+min_allowed_chance = 0.95
 
 def reset_cb():
     st.session_state['how_many_bolts'] = ""
-    del st.session_state['answer']
+    del st.session_state['chance_all_good']
+    del st.session_state['answer_text']
 
 st.title("Bolt Weight Calculator")
 n_bolts = st.text_input("How many bolts?",
@@ -41,12 +43,25 @@ def calc_prob(*args, **kwargs):
                 pass
     if len(all_entries) == n_samps:
         samps = np.array(all_entries)
-        est_mean = samps.mean()
-        est_stdv = np.std(samps, ddof=1)
-        est_dist = stats.norm(est_mean, est_stdv)
-        odds_per_bolt = 1.0 - est_dist.cdf(max_wt)
-        odds_all_good = math.pow(1.0 - odds_per_bolt, n_bolts)
-        st.session_state['answer'] = odds_all_good
+        if any([samp >= max_wt for samp in samps]):
+            st.session_state['chance_all_good'] = 0.0
+            st.session_state['answer_text'] = ("This sample fails the test."
+                                               " One of the samples you weighed"
+                                               " was above the limit.")
+        else:
+            est_mean = samps.mean()
+            est_stdv = np.std(samps, ddof=1)
+            est_dist = stats.norm(est_mean, est_stdv)
+            odds_per_bolt = 1.0 - est_dist.cdf(max_wt)
+            odds_all_good = math.pow(1.0 - odds_per_bolt, n_bolts)
+            st.session_state['chance_all_good'] = odds_all_good
+            if odds_all_good >= min_allowed_chance:
+                st.session_state['answer_text'] = ("These samples pass the"
+                                                   " statistical test.")
+            else:
+                st.session_state['answer_text'] = ("These samples fail the"
+                                                   " statistical test. You need"
+                                                   " to weigh all the bolts.")
     
 try:
     if n_bolts:
@@ -75,9 +90,11 @@ if st.session_state['how_many_bolts']:
                                                           "n_samps": n_samps,
                                                           "n_bolts": n_bolts},
                                                   args=[elt, key])
-        if 'answer' in st.session_state:
-            pct_str = "{:2.1%}".format(st.session_state['answer'])
+        if 'chance_all_good' in st.session_state:
+            pct_str = "{:2.1%}".format(st.session_state['chance_all_good'])
             st.subheader(f"The chance that all bolts are below the limit is {pct_str}")
+        if 'answer_text' in st.session_state:
+            st.subheader(f"{st.session_state['answer_text']}")
     
 
 
